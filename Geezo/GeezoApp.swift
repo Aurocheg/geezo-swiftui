b2b27b4e292f435dbbe55ec8745c6f13293169dd
Ivan Nichipor <aurocheg@gmail.com>
2023-06-02 19:32:58 +0300

    add firebase to project

--- Geezo/GeezoApp.swift
+++ Geezo/GeezoApp.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct GeezoApp: App {
-    let persistenceController = PersistenceController.shared
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    private let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
